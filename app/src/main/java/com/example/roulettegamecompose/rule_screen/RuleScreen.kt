3daa3f72552bd56e7ead650492b6d1fcb238a2d7
MikleRuk <MikleRuk>
2023-08-31 10:14:22 +0300

    Initial commit

---
+++ app/src/main/java/com/example/roulettegamecompose/rule_screen/RuleScreen.kt
@@ -0,0 +1,119 @@
+package com.example.roulettegamecompose.rule_screen
+
+import androidx.compose.animation.core.LinearEasing
+import androidx.compose.animation.core.LinearOutSlowInEasing
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.offset
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.layout.wrapContentWidth
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.rotate
+import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.roulettegamecompose.R
+import com.example.roulettegamecompose.utils.NumberUtil
+import kotlin.math.roundToInt
+
+@Preview
+@Composable
+fun RuleScreen() {
+
+    var rotationValue by remember {
+        mutableStateOf(0f)
+    }
+
+    var number by remember {
+        mutableStateOf(0)
+    }
+
+    val angle: Float by animateFloatAsState(
+        targetValue = rotationValue,
+        animationSpec = tween(
+            durationMillis = 3000,
+            easing = LinearOutSlowInEasing
+        ),
+        label = "",
+        finishedListener = {
+
+            val index = (360f - (it % 360)) / (360f / NumberUtil.list.size)
+            number = NumberUtil.list[index.roundToInt()]
+
+        }
+    )
+
+
+
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        verticalArrangement = Arrangement.SpaceBetween
+    ) {
+        Text(
+            modifier = Modifier
+                .fillMaxWidth()
+                .wrapContentHeight()
+                .wrapContentWidth(),
+            text = number.toString(),
+            fontWeight = FontWeight.Bold,
+            fontSize = 35.sp,
+            color = Color.White
+        )
+
+        Box(
+            modifier = Modifier
+                .weight(1f)
+                .fillMaxSize()
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.ruleta),
+                contentDescription = "ruletka",
+                modifier = Modifier
+                    .fillMaxSize()
+                    .rotate(angle)
+            )
+            Image(
+                painter = painterResource(id = R.drawable.flecha),
+                contentDescription = "flecha",
+                modifier = Modifier.fillMaxSize()
+            )
+        }
+
+        Button(
+            onClick = {
+                rotationValue = (720..1080).random().toFloat() + angle
+            },
+            colors = ButtonDefaults.buttonColors(Color.Red),
+            modifier = Modifier
+                .fillMaxWidth()
+                .offset(0.dp, -10.dp)
+        ) {
+            Text(
+                text = "Start",
+                color = Color.White,
+                fontSize = 22.sp
+
+            )
+        }
+    }
+}
